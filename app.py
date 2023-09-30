b7daa9740c8876ddd4c17a934ede2b8d4470a874
Unnati101 <143032635+Unnati101@users.noreply.github.com>
2023-09-30 14:00:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import gradio as gr
+import cv2
+
+def face():
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    cap = cv2.VideoCapture(0)
+    cap.set(3,640) # set Width
+    cap.set(4,480) # set Height
+    while True:
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,     
+            scaleFactor=1.2,
+            minNeighbors=5,     
+            minSize=(20, 20)
+        )
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+            cv2.putText(img, str("Face"),(x+5,y-5),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)      
+        cv2.imshow('video',img)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27: # press 'ESC' to quit
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+
+
+
+    
+
+with gr.Blocks() as app:
+    greet_btn = gr.Button("start face detection")
+    greet_btn.click(fn=face, inputs=None,  outputs=None, api_name="face-detection")
+    
+app.launch(share=True)   
